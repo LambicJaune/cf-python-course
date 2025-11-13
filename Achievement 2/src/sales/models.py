44f66f17647b04a4237183272e165cb1931ae79a
LambicJaune <thehopman@outlook.com>
2025-11-13 15:36:44 +0100

    Added Practice task Bookstore

---
+++ Achievement 2/src/sales/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from books.models import Book
+
+# Create your models here.
+
+class Sale(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.FloatField()
+    models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return str(self.name)
