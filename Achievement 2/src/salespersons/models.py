5179bc8bfc1353f90aa2588f3120769c529d3c10
LambicJaune <thehopman@outlook.com>
2025-11-26 13:59:10 +0100

    Added Practice Task Exercise 2.5

--- Achievement 2/src/salespersons/models.py
+++ Achievement 2/src/salespersons/models.py
@@ -7,3 +7,4 @@ class Salesperson(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=120)
     bio = models.TextField(default="no bio...")
+    pic = models.ImageField(upload_to='salespersons', default='no_picture.jpg')
