1052de9e12d5de03be19861fc2cd2c7555a924cc
LambicJaune <thehopman@outlook.com>
2025-12-16 11:57:53 +0100

    Added practice task 2.7 and learning journal
    + answers doc to the main task

--- Achievement 2/src/sales/models.py
+++ Achievement 2/src/sales/models.py
@@ -7,8 +7,8 @@ class Sale(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.FloatField()
-    models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return str(self.name)
+        return f"id: {self.id}, book: {self.book.name}, quantity: {self.quantity}, price: {self.price}"
